014a63219b0a502eda95c7ef6b76de89b7b22491
ssh1202 <tmdghks9812@gmail.com>
2025-12-31 09:07:41 +0900

    otherscodekata

---
+++ CodekataOthers/CodekataDay6/day6.cpp
@@ -0,0 +1,37 @@
+//나머지가 1이 되는 수 찾기
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 1)
+        {
+            return i;
+        }
+    }
+
+}
+//x만큼 간격이 있는 n개의 숫자
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+
+    int num = x;
+
+    for (int i = 0; i < n; i++)
+    {
+        num = x + i * x;
+        answer.push_back(num);
+    }
+
+    return answer;
+}
