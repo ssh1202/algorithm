7a568214964b428e502314ce066e37d80d4620c6
ssh1202 <tmdghks9812@gmail.com>
2026-01-02 09:06:01 +0900

    day7

--- CodekataOthers/CodekataDay6/day6.cpp
+++ CodekataOthers/CodekataDay6/day6.cpp
@@ -35,3 +35,17 @@ vector<long long> solution(int x, int n) {
 
     return answer;
 }
+//자연수 뒤집어 배열로 만들기
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    while (n > 0) {
+        answer.push_back(n % 10);
+        n /= 10;
+    }
+    return answer;
+}
