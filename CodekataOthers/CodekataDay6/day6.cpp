daa16297caa0991d58f274e42d5bc76ecb31f17c
ssh1202 <tmdghks9812@gmail.com>
2026-01-13 11:49:28 +0900

    01.13

--- CodekataOthers/CodekataDay6/day6.cpp
+++ CodekataOthers/CodekataDay6/day6.cpp
@@ -119,3 +119,28 @@ long long solution(int a, int b) {
         answer += i;
     return answer;
 }
+
+
+//콜라츠 추측 (01.02)
+int solution(int num) {
+    long long n = num; 
+    int count = 0;
+
+    if (n == 1)
+        return 0;
+
+    while (n != 1 && count < 500) {
+        if (n % 2 == 0) {
+            n /= 2;
+        }
+        else {
+            n = n * 3 + 1;
+        }
+        count++;
+    }
+
+    if (n == 1)
+        return count;
+    else
+        return -1;
+}
