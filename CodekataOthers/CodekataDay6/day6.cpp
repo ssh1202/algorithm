024c3543b63ac31895ac624a7c7ae5f30dd69db3
ssh1202 <tmdghks9812@gmail.com>
2026-01-14 12:45:30 +0900

    0114

--- CodekataOthers/CodekataDay6/day6.cpp
+++ CodekataOthers/CodekataDay6/day6.cpp
@@ -144,3 +144,17 @@ int solution(int num) {
     else
         return -1;
 }
+//서울에서 김서방 찾기
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    for (int i = 0; i < seoul.size(); i++) {
+        if (seoul[i] == "Kim")
+            answer = "김서방은 " + to_string(i) + "에 있다";
+    }
+    return answer;
+}
