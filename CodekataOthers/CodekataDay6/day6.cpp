8df2c0e40519e96f986f007914d20250c3080b22
ssh1202 <tmdghks9812@gmail.com>
2026-01-16 11:26:26 +0900

    01.16

--- CodekataOthers/CodekataDay6/day6.cpp
+++ CodekataOthers/CodekataDay6/day6.cpp
@@ -173,3 +173,18 @@ vector<int> solution(vector<int> arr, int divisor) {
     sort(answer.begin(), answer.end());
     return answer;
 }
+//음양 더하기
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < signs.size(); i++) {
+        if (!signs[i]) absolutes[i] *= -1;
+        answer += absolutes[i];
+    }
+    return answer;
+}
+//
